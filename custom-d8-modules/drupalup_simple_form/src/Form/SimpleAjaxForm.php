29af292222f1e5f97b81f453ecd96f2549ff9221
Kiran Dangol <kiran_dan2009@yahoo.com>
2019-12-26 13:39:10 -0600

    customized ajax implementation for drupal 8 simple forms

--- custom-d8-modules/drupalup_simple_form/src/Form/SimpleAjaxForm.php
+++ custom-d8-modules/drupalup_simple_form/src/Form/SimpleAjaxForm.php
@@ -24,7 +24,7 @@ class SimpleAjaxForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    $form['massage'] = [
+    $form['message'] = [
       '#type' => 'markup',
       '#markup' => '<div class="result_message"></div>',
     ];
@@ -37,6 +37,41 @@ class SimpleAjaxForm extends FormBase {
     $form['number_2'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Second number'),
+    ];
+
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Whats your name?'),
+    ];
+
+    $form['phone_number'] = [
+      '#type' => 'tel',
+      '#title' => $this->t('Whats your number?'),
+    ];
+
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('How do I email you?'),
+    ];
+
+    $form['date'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Date today?'),
+    ];
+
+    $form['url'] = [
+      '#type' => 'url',
+      '#title' => $this->t('Whats your website [URL]:'),
+    ];
+
+    $form['search'] = [
+      '#type' => 'search',
+      '#title' => $this->t('Wanna look up sth?'),
+    ];
+
+    $form['range'] = [
+      '#type' => 'range',
+      '#title' => $this->t('Whatchoo ranging for?'),
     ];
 
     $form['actions'] = [
@@ -56,8 +91,26 @@ class SimpleAjaxForm extends FormBase {
     $response->addCommand(
       new HtmlCommand(
         '.result_message',
-        '<div class="my_top_message">' . $this->t('The result is @result', ['@result' => ($form_state->getValue('number_1') + $form_state->getValue('number_2'))])
+        '<div class="my_top_message">' 
+        .'</br>' 
+        .$this->t("Thank you @name !", [ '@name' => ($form_state->getValue('name')) ])
+        .'</br>' 
+        .$this->t("Your email is @email", [ '@email' => ($form_state->getValue('email')) ])
+        .'</br>' 
+        .$this->t("Todays date is @date", [ '@date' => ($form_state->getValue('date')) ])
+        .'</br>' 
+        .$this->t("Your contact number is @number", [ '@number' => ($form_state->getValue('phone_number')) ])
+        .'</br>' 
+        .$this->t("Your website is @web", [ '@web' => ($form_state->getValue('url')) ])
+        .'</br>' 
+        . $this->t('And btw, sum of @num1 and @num2 is @result', 
+          [
+            '@num1' => ($form_state->getValue('number_1')),
+            '@num2' => ($form_state->getValue('number_2')),
+            '@result' => ($form_state->getValue('number_1') + $form_state->getValue('number_2'))
+          ]
         )
+      ) 
     );
 
     return $response;
